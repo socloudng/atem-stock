289336a24258825910001af3876ab6f34fb798e2
songzb <ada223ada@qq.com>
2022-04-14 14:16:15 +0800

    修改一个地址

--- remote/stock_remote_url.go
+++ remote/stock_remote_url.go
@@ -1,6 +1,8 @@
 package remote
 
-import "strconv"
+import (
+	"strconv"
+)
 
 func getUrlOfSohuUrl(stockCode string, startDate string, endDate string) string {
 	return "http://q.stock.sohu.com/hisHq?" +
@@ -19,13 +21,12 @@ func getStockListPageUrl(pageSize int, pageNo int) string {
 		"&pn=" + strconv.Itoa(pageNo)
 }
 
-//fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152
-func getStockDailyDataListPageUrl(pageSize int, pageNo int) string {
-	return "https://push2.eastmoney.com/api/qt/clist/get?" +
-		"fields=f2,f3,f5,f6,f15,f16,f17" +
-		"&fs=m:0+t:6+f:!2,m:0+t:13+f:!2,m:0+t:80+f:!2,m:1+t:2+f:!2,m:1+t:23+f:!2,m:0+t:7+f:!2,m:1+t:3+f:!2" +
-		"&pz=" + strconv.Itoa(pageSize) +
-		"&pn=" + strconv.Itoa(pageNo)
+func getStockDailyDataListPageUrl(code, fromDate, endDate string) string {
+
+	return "http://app.finance.ifeng.com/hq/stock_daily.php?" +
+		"code=" + code +
+		"&begin_day" + fromDate +
+		"&end_day=" + endDate
 }
 
 func getStockListHtmlUrl() (url string, search string) {
